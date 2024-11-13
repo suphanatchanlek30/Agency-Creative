67b6d25013a21f25c0d2a494d169866787adaf3e
suphanatchanlek <suphanatchanlek@gmail.com>
2024-11-13 14:51:08 +0700

    add Responsive for Services

--- src/page/Services.jsx
+++ src/page/Services.jsx
@@ -16,28 +16,34 @@ const Services = () => {
       ];
 
     return (
-        <div className='flex flex-col justify-center container mx-auto mt-[200px] mb-[58px] w-[1240px] h-[568px]'>
+        <div className='container mx-auto px-4 
+                    flex flex-col justify-center
+                    mt-80 sm:mt-12 md:mt-[58px] 
+                    pt-8 sm:pt-12 md:pt-[100px] 
+                    mb-[150%] sm:mb-12 md:mb-[58px] 
+                    w-full sm:max-w-3xl md:max-w-4xl lg:max-w-6xl xl:w-[1240px]
+                    min-h-[400px] sm:min-h-[500px] md:min-h-[568px]'>
             {/* right left */}
-            <div className='flex h-screen mt-6 justify-center gap-6'>
+            <div className='flex sm:flex-row flex-col h-screen mt-6 justify-center sm:gap-6 gap-0'>
                 {/* left */}
-                <div className='left w-1/2'>
+                <div className='left sm:w-1/2 w-full sm:text-start text-center'>
                     {/* text */}
-                    <div className='text w-[521px] h-[414px] mt-[60px]'>
-                        <p className='font-body font-semibold text-[24px] text-[#377DFF] tracking-[0.01em] leading-[24px]'>Our Services</p>
-                        <h2 className='font-body2 font-bold text-[55px] text-[#1D1D1D] tracking-[0.003em] leading-[1.3em] mt-[32px]'>Perfect and Fast Movement</h2>
-                        <p className='font-body font-normal text-[20px] text-[#464646] tracking-[0.01em] leading-[32px] mt-[18px]'>We move with make a Creative Strategy for help your business goal, we help to improve your income by a services we have. make your content look interesting and make people look for your business</p>
+                    <div className='text w-[330px] sm:w-[521px] h-[414px] sm:mt-[60px] mt-[50%]'>
+                        <p className='font-body font-semibold text-[16px] sm:text-[24px] text-[#377DFF] tracking-[0.01em] leading-[24px]'>Our Services</p>
+                        <h2 className='font-body2 font-bold text-[24px] sm:text-[55px] text-[#1D1D1D] tracking-[0.003em] leading-[1.3em] mt-[32px]'>Perfect and Fast Movement</h2>
+                        <p className='font-body font-normal text-[14px] sm:text-[20px] text-[#464646] tracking-[0.01em] leading-[32px] mt-[18px]'>We move with make a Creative Strategy for help your business goal, we help to improve your income by a services we have. make your content look interesting and make people look for your business</p>
 
                         {/* icon and text */}
-                        <a href="#" className='icon_text flex justify-end items-center gap-[16px] mt-[36px]'>
-                            <p className='font-body font-bold text-[20px] text-[#377DFF] tracking-[0.01em] leading-[34px]'>Read more</p>
-                            <GoArrowRight className='text-[#377DFF] text-[1.5rem]' />
+                        <a href="#" className='icon_text flex justify-start sm:justify-end items-center gap-[16px] mt-[36px]'>
+                            <p className='font-body font-bold text-[14px] sm:text-[20px] text-[#377DFF] tracking-[0.01em] leading-[34px]'>Read more</p>
+                            <GoArrowRight className='text-[#377DFF] text-[1rem] sm:text-[1.5rem]' />
                         </a>
                     </div>
                 </div>
 
                 {/* right */}
-                <div className='right w-1/2'>
-                    <div className='grid grid-cols-2 gap-y-[30px] gap-x-[30px] ml-10'>
+                <div className='right sm:w-1/2 w-full'>
+                    <div className='grid sm:grid-cols-2 grid-cols-1 sm:gap-y-[10%] sm:gap-x-[20%] gap-y-[4%] sm:ml-10 ml-10'>
                         {services.map(service => (
                             <div key={service.id} className='flex flex-col items-center justify-center border bg-[#FFFFFF] w-[262px] h-[269px] rounded-[30px] duration-300 hover:shadow-[3px_3px_15px] hover:shadow-[#e1e1e1]'>
                                 <div className={`text-white w-[100px] h-[100px] rounded-full ${service.bgColor}`}>
@@ -45,7 +51,7 @@ const Services = () => {
                                         {service.icon}
                                     </div>
                                 </div>
-                                <h3 className='font-body text-[24px] font-bold mt-[32px] text-center text-[#1D1D1D] tracking-[0.01em] leading-[34px]'>{service.title}</h3>
+                                <h3 className='font-body w-[140px] sm:w-full sm:text-[24px] text-[16px] font-bold mt-[32px] text-center text-[#1D1D1D] tracking-[0.01em] leading-[34px]'>{service.title}</h3>
                             </div>
                         ))}
                     </div>
